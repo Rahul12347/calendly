0df979cdd5aad2281112f22014b325f3f562f018
Rahul12347 <2019rahul.maurya@ves.ac.in>
2024-09-26 21:08:32 +0530

    changes made

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
+import CreateEventDrawer from "@/components/create-event";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import Header from "@/components/header";
-import CreateEventDrawer from "@/components/create-event";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
           </main>
           <footer className="bg-blue-100 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Made by Me</p>
             </div>
           </footer>
           <CreateEventDrawer />
